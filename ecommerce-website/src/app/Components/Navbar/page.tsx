605478a76b82da62bc50786900a3048025bd0651
Muhammaddanish <muhammaddanishgtr24@gmail.com>
2024-10-20 02:56:56 +0500

    Add logo direct from folder in public folder

---
+++ ecommerce-website/src/app/Components/Navbar/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='navbar'>
+<div>
+  <img src="/logo.jpg" alt="abc" height="200px" />
+</div>
+</nav>
+  )
+}
+
+export default Navbar
---
+++ ecommerce-website/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./Components/Navbar/page";
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div><Navbar/></div>
+  )
+}
+
+export default Home
